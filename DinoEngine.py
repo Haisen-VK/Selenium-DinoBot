ed169bd6e3d9a25c80b35c9e81a31a3328d47456
Haisen_k <vkrizwan@gmail.com>
2023-03-06 10:45:54 +0530

    ObstacleScanner

--- DinoEngine.py
+++ DinoEngine.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
 import os
+import time
 
 load_dotenv('.env') 
 
@@ -20,6 +21,7 @@ class DinoEngine:
            self.Driver.get("chrome://dino/")
         except WebDriverException:
             self.HitSpace()
+            self.ScanObstacles()
             pass
         #Following Code Helps Browser to Keep Running and not Quit
         while(True):
@@ -29,3 +31,13 @@ class DinoEngine:
         #Hits Space
         self.Actions.send_keys(Keys.SPACE)
         self.Actions.perform()
+
+
+    def ScanObstacles(self):
+        self.PastObstacles = []
+        while(True):
+            self.Obstacles  = self.Driver.execute_script("return Runner.instance_.horizon.obstacles")
+            if(self.Obstacles!=[] and self.Obstacles!=self.PastObstacles):
+                self.PastObstacles=self.Obstacles
+                self.Driver.execute_script("console.log(Runner.instance_.horizon.obstacles)")
+                time.sleep(1)
