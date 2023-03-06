3e92b308b1c1c0fa7e6eb8e4c7660833c748463e
Haisen_k <vkrizwan@gmail.com>
2023-03-06 17:15:19 +0530

    DinoBot v1.0

--- DinoBot.py
+++ DinoBot.py
@@ -1,7 +1,10 @@
+import os
 from DinoEngine import DinoEngine
+from dotenv import load_dotenv
 """
     Must include .env file with DRIVERPATH                  
 
 """
-Dino = DinoEngine()
+load_dotenv('.env') 
+Dino = DinoEngine(os.environ.get("DRIVERPATH"),os.environ.get("OBSTACLE_DETECTION_RANGE"))
 Dino.Initialize()
--- DinoEngine.py
+++ DinoEngine.py
@@ -1,40 +1,40 @@
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
-import os
-import time
 
-load_dotenv('.env') 
 
 class DinoEngine:
-    def __init__(self):
+    def __init__(self,PATH,OBSTACLE_DETECTION_RANGE):
         #initialize Basic Things(Gets Path from .env,etc)
-        self.PATH = os.environ.get("DRIVERPATH")
+        self.PATH = PATH
+        self.OBSTACLE_DETECTION_RANGE = int(OBSTACLE_DETECTION_RANGE)
         self.Driver = webdriver.Chrome(self.PATH)
         self.Actions = ActionChains(self.Driver)
-
+        self.tRex = object
+        self.nxtObstacles  = object
+        self.PastObstacles = []
+        
     def Initialize(self):
         #Code To Kickstart chrome dino game
         try:
            self.Driver.get("chrome://dino/")
         except WebDriverException:
-            self.HitSpace()
-            self.ScanObstacles()
-            pass
-        #Following Code Helps Browser to Keep Running and not Quit
+             pass
+        self.HitKey(Keys.SPACE)
+        self.ScanObstacles()
         while(True):
             pass
-   
-    def HitSpace(self):
+        
+
+    def HitKey(self,Key):
         #Hits Space
-        self.Actions.send_keys(Keys.SPACE)
-        self.Actions.perform()
+       
+            self.Actions.key_down(Key).pause(0.1).key_up(Key).perform()
 
 
     def ScanObstacles(self):
-        self.PastObstacles = []
+        
         while(True):
           
             self.tRex = self.Driver.execute_script("return Runner.instance_.tRex")
@@ -42,15 +42,16 @@ class DinoEngine:
             if(self.nxtObstacles!=[] and self.nxtObstacles!=self.PastObstacles):
                 self.PastObstacles=self.nxtObstacles
                 self.Driver.execute_script("console.log(Runner.instance_.horizon.obstacles)")
-                self.action()
-    def action(self):
+                self.SelectState()
+    def SelectState(self):
                    
 
-        	if(self.nxtObstacles and ( self.nxtObstacles['xPos'] - self.tRex['xPos'] ) <= 150 ):
+        	if(self.nxtObstacles and ( self.nxtObstacles['xPos'] - self.tRex['xPos'] ) <=  self.OBSTACLE_DETECTION_RANGE):
                     if(self.nxtObstacles["typeConfig"]["type"] == "CACTUS_LARGE" or self.nxtObstacles["typeConfig"]["type"] == "CACTUS_SMALL" or (self.nxtObstacles["typeConfig"]["type"] == "PTERODACTYL" and self.nxtObstacles['yPos']>=75)):
-                        self.Driver.execute_script("Runner.instance_.tRex.startJump(0)")
+                        self.HitKey(Keys.SPACE)
                     if(self.nxtObstacles["typeConfig"]["type"] == "PTERODACTYL" and self.nxtObstacles['yPos']<75):
-                        self.Driver.execute_script("Runner.instance_.tRex.setDuck(true)")
+                        self.HitKey(Keys.ARROW_DOWN)
+                        
                      
                         
 	
